86ffcc0793714f3a5ee7da3db8707559e94f6ed2
younes-bami <35866862+younes-bami@users.noreply.github.com>
2024-06-26 16:33:02 +0100

    Create jwt.stfategy.ts

---
+++ cfinder/backend/src/users/jwt.stfategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from './users.service';
+import { JwtPayload } from './jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly usersService: UsersService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'secretKey', // Remplacez par votre clé secrète
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.usersService.findOne(payload.username);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
