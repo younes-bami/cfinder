f1305fb7d09fd9292e0e5f71d498e5d07e19d1cd
younes-bami <35866862+younes-bami@users.noreply.github.com>
2024-06-28 16:31:09 +0100

    Update app.module.ts

--- cfinder/backend/src/app.module.ts
+++ cfinder/backend/src/app.module.ts
@@ -4,7 +4,7 @@ import { UsersModule } from './users/users.module';
 import { CoachesModule } from './coaches/coaches.module';
 import { BookingsModule } from './bookings/bookings.module';
 
-Module({
+@Module({
   imports: [
     MongooseModule.forRoot('mongodb://mongo:27017/sports-coaching-app'),
     UsersModule,
